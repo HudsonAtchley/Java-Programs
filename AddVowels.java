67a9eb876bcfd38119ce6941b1d29e33e7ca090b
Hudson Atchley <117960528+HudsonAtchley@users.noreply.github.com>
2023-01-29 20:34:43 -0600

    Add files via upload

---
+++ AddVowels.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class AddVowels 
+{
+	public static void main(String[] args) 
+	{
+		Scanner kb = new Scanner(System.in);
+		ArrayList<String> text = new ArrayList<String>();
+
+		System.out.print("Enter a word: ");
+		String word = kb.next();		
+
+		//missing code
+		for(int i = 0; i < word.length(); i++)
+		{
+			char eachChar = word.charAt(i);
+			String letter = Character.toString(eachChar);
+			
+			if(letter.equals("a")||letter.equals("e")||letter.equals("i")||letter.equals("o")||letter.equals("u"))
+			{
+
+			text.add(letter);
+			}
+			text.add(letter);
+		}
+    
+		for(String s:text)
+		{
+
+			System.out.print(s);	
+	
+		}
+		System.out.println();
+	}
+}
